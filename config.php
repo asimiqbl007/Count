f1f58c585fd8f7f43a08cff67a66182ff11743c7
asimiqbl007 <asimiqbl007@hotmail.com>
2015-12-31 04:18:57 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 class Config {
 	/* global cache settings */
-    public $cache = '';          // 'memcache', 'apc' or 'file' - leave blank to disable caching
+    public $cache = 'memcache';          // 'memcache', 'apc' or 'file' - leave blank to disable caching
     public $cache_time = 86400;  // in seconds e.g. 86400 = 24h
     
     /* memcache settings - only required if cache set to memcache */
-    public $cache_server = 'http://192.241.228.57';
+    public $cache_server = 'localhost';
     public $cache_port = 42;
     public $cache_persistent = true;
     
@@ -13,10 +13,10 @@ class Config {
     public $cache_directory = '/blstat/'; // needs trailing slash!
     
     /* database settings - only required to track shares */
-    public $db_type = 'mysql';
-    public $db_host = 'localhost';
-    public $db_name = 'blstat';
-    public $db_user = 'root';
+    public $db_type = '';
+    public $db_host = '';
+    public $db_name = '';
+    public $db_user = '';
     public $db_pass = '';
     
     /* default output settings - ignored if url parameter has been passed */
